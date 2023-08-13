eb654be97fb077ed7093a143948fd0fabbb439db
CoolHandN <36666301+CoolhandinMotion@users.noreply.github.com>
2023-08-13 18:12:20 +0200

    added the __call__ for control module in order to show the type hints. did the mapping alias for navigation dictionaries.

--- components/control.py
+++ components/control.py
@@ -1,20 +1,26 @@
+from typing import Protocol, Dict
 
 import pygame
 from components.navigation import Navigation
 from components.spaceships import shoot
-from components.spaceships import Spaceship,Appearance,State,SpaceShipType
+from components.spaceships import Spaceship, Appearance, State, SpaceShipType
+
+
+class NavigationCommand(Protocol):
+    def __call__(self, app: Appearance, state: State):
+        ...
 
 
 
 
-RED_NAVIGATION_COMMAND = {
+RED_NAVIGATION_COMMAND: Dict[int, NavigationCommand] = {
     pygame.K_UP: Navigation.move_up,
     pygame.K_DOWN: Navigation.move_down,
     pygame.K_RIGHT: Navigation.move_right,
     pygame.K_LEFT: Navigation.move_left,
 
 }
-YELLOW_NAVIGATION_COMMAND = {
+YELLOW_NAVIGATION_COMMAND: Dict[int, NavigationCommand] = {
     pygame.K_w: Navigation.move_up,
     pygame.K_s: Navigation.move_down,
     pygame.K_d: Navigation.move_right,
@@ -22,21 +28,20 @@ YELLOW_NAVIGATION_COMMAND = {
 
 }
 
-RED_SHOOT_COMMAND={pygame.K_RCTRL:shoot }
-YELLOW_SHOOT_COMMAND={pygame.K_LCTRL: shoot}
+RED_SHOOT_COMMAND = {pygame.K_RCTRL: shoot}
+YELLOW_SHOOT_COMMAND = {pygame.K_LCTRL: shoot}
 
 
-def handle_keys_pressed(keys_pressed, red:Spaceship,yellow:Spaceship):
+def handle_keys_pressed(keys_pressed, red: Spaceship, yellow: Spaceship):
     for command in RED_NAVIGATION_COMMAND:
         if keys_pressed[command]:
-            RED_NAVIGATION_COMMAND[command](app=red.appearance,state=red.state)
+            RED_NAVIGATION_COMMAND[command](app=red.appearance, state=red.state)
     for command in YELLOW_NAVIGATION_COMMAND:
         if keys_pressed[command]:
-            YELLOW_NAVIGATION_COMMAND[command](app=yellow.appearance,state =yellow.state )
+            YELLOW_NAVIGATION_COMMAND[command](app=yellow.appearance, state=yellow.state)
     for command in RED_SHOOT_COMMAND:
         if keys_pressed[command]:
             RED_SHOOT_COMMAND[command](spaceship=red)
     for command in YELLOW_SHOOT_COMMAND:
         if keys_pressed[command]:
             YELLOW_SHOOT_COMMAND[command](spaceship=yellow)
-
